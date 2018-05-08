eda2674d35439a4ae9b172bc0f2e0deb90dec51c
B.frolikov <b.frolikov@gmail.com>
2018-05-08 14:09:58 +0300

    za

--- analyzeall.py
+++ analyzeall.py
@@ -3,20 +3,52 @@ import librosa.display
 import numpy as np
 import analyzer
 import glob
+from numpy import linalg as LA
+from mpl_toolkits import mplot3d
+import matplotlib.pyplot as plt
+
+f = open('za.out', 'w')
+def analyze(args) : 
+    plt.figure()
+    for j, (data, name) in enumerate(args):
+        data = np.abs(data)
+
+        f.write(name + "\n")
+
+        for g in range(len(data[0])) : 
+            for k in range(len(data)) :
+                f.write("{} ".format(str(data[k][g]), name))
+            f.write("\n")
+        
+
+        #data = data/LA.norm(data, np.inf)
+
+        # for i in data : 
+        #     indexes = i < 0.03
+        #     i[indexes] = 0 
+
+        # print(args)
+
+        plt.subplot(len(args), 1, j+1)
+        # librosa.display.specshow(librosa.amplitude_to_db(data, ref=np.max), y_axis='log')
+        # plt.colorbar(format='%+2.0f dB')
+        plt.plot(data)
+        plt.title(name)
+        
+
 
 data = []
 
-for i, filename in enumerate(glob.iglob('*.mp3')):
+for i, filename in enumerate(glob.iglob('*s?.mp3')):
     
 
     y, sr = librosa.load(filename)
-    D = librosa.stft(y, hop_length=128)
+    D = librosa.stft(y, hop_length=32)
     
     data.append((D, filename))
 
-    if i % 4 == 0 : 
-        analyzer.analyze(data)
+    if i % 4 == 3 : 
+        analyze(data)
         data = []
-
-
-    
+plt.show()
+f.close()
